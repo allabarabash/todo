5e5d3631d2b00fd86856c470487ebf8d70c8a294
alla <mail@barabash.dev>
2021-01-27 18:24:38 +0100

    basic functions without filter

---
+++ src/components/app/app.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from "../item-status-filter";
+
+import './app.css';
+
+export default class App extends Component {
+    state = {
+        todoData: [
+            { label: 'Drink Coffee', important: false, id: 1 },
+            { label: 'Make Awesome App', important: false, id: 2 },
+            { label: 'Have a lunch', important: false, id: 3 }
+        ]
+    }
+
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = this.state.todoData.findIndex( (el) => el.id === id )
+            console.log(idx)
+        })
+    }
+
+    render () {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={ this.deleteItem }/>
+            </div>
+        )
+
+    }
+}
