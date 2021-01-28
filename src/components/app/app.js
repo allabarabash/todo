d6845899bd7d74af4cbfc77846f8aa5813b77f47
alla <a.barabasch@gmail.com>
2021-01-28 23:37:04 +0100

    completed delete function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,14 +12,30 @@ export default class App extends Component {
         todoData: [
             { label: 'Drink Coffee', important: false, id: 1 },
             { label: 'Make Awesome App', important: false, id: 2 },
-            { label: 'Have a lunch', important: false, id: 3 }
+            { label: 'Have a lunch', important: false, id: 3 },
+            { label: 'Свободу Навальному', important: false, id: 4 },
+            { label: 'Парам пам пам', important: false, id: 5 },
+            { label: 'Ещё что-то, забылъ', important: false, id: 6 }
         ]
     }
 
     deleteItem = (id) => {
         this.setState(({ todoData }) => {
             const idx = this.state.todoData.findIndex( (el) => el.id === id )
-            console.log(idx)
+
+            // [a, b, c, d, e]
+            // [a, b,    d, e]
+            const before = todoData.slice(0, idx)
+            console.log(before)
+            const after = todoData.slice(idx + 1)
+            console.log(after)
+            const newArray = [
+                ...before,
+                ...after]
+
+            return {
+                todoData: newArray
+            }
         })
     }
 
